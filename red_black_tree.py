f2080a133872a91486a4b9fc507bd50563f451e8
changken <pp24513@gmail.com>
2023-03-27 22:25:18 +0800

    rbt 新增

--- red_black_tree.py
+++ red_black_tree.py
@@ -1,15 +1,16 @@
 class TreeNode:
-    def __init__(self, data):
+    def __init__(self, key, strs):
         self.left = None
         self.right = None
-        self.info = data
+        self.key = key  # node's key
+        self.strs = strs  # node's string data
         self.color = False  # False -> Red, True -> Black
 
 
 class RedBlackTree:
     def __init__(self):
         self.root = None
-        self.neel = TreeNode(None)
+        self.neel = TreeNode(None, None)
 
     def left_rotation(self, x):
         y = x.right  # 把y指向x的rightchild, 最後y會變成x的parent
@@ -44,9 +45,81 @@ class RedBlackTree:
             self.root = x
         elif y == y.parent.left:  # 若原先y是其parent之leftchild,
             y.parent.left = x  # x就成為其新的parent之leftchild
-        else: # 若原先y是其parent之rightchild, 
+        else:  # 若原先y是其parent之rightchild,
             y.parent.right = x  # x就成為其新的parent之rightchild
 
         x.right = y  # 將y設為x之rightchild
-        y.parent = x # 將x設為y之parent
-    
+        y.parent = x  # 將x設為y之parent
+
+    def insert(self, key, strs):
+        # 前半部與 InsertBST()之邏輯完全相同, 僅僅需要修改 NULL <-> NIL
+        y = self.neel  # root的上一個
+        x = self.root  # 初始為root
+        insert_node = TreeNode(key, strs)
+
+        while x != self.neel:  # 把root初始化成neel, 這裡就可以用neel來做判斷
+            y = x
+            if insert_node.key < x.key:
+                x = x.left
+            else:
+                x = x.right
+
+        insert_node.parent = y
+
+        if y == self.neel:
+            self.root = insert_node
+        elif insert_node.key < y.key:
+            y.left = insert_node
+        else:
+            y.right = insert_node
+
+        # 以下是對RBT之node的設定, 將child pointer指向NIL, 顏色設為紅色
+        insert_node.left = self.neel
+        insert_node.right = self.neel
+        insert_node.color = False  # 顏色可以在constructor中預設
+
+        self.insert_fixup(insert_node)  # 對可能出現紅色與紅色node相連之情形做修正
+
+    def insert_fixup(self, current):
+        # case0: parent是黑色, 就不用進回圈
+        while(current.parent.color == False):  # 若parent是紅色即進入迴圈
+            # 上半部：parent是grandparent的left child
+            if current.parent == current.parent.parent.left:
+                uncle = current.parent.parent.right
+
+                # case1: 若uncle是紅色
+                if uncle.color == False:
+                    current.parent.color = True
+                    uncle.color = True
+                    current.parent.parent.color = False  # grandparent改成紅色
+                    current = current.parent.parent
+                # case2 & 3: uncle是黑色
+                else:
+                    if current == current.parent.right:  # case2
+                        current = current.parent
+                        self.left_rotation(current)
+                    # case3
+                    current.parent.color = True  # 把parent塗黑
+                    current.parent.parent.color = False  # grandparent塗紅
+                    self.right_rotation(current.parent.parent)
+            # 下半部：parent是grandparent的right child, 與上半部對稱
+            else:
+                uncle = current.parent.parent.left
+
+                # case1: 若uncle是紅色
+                if uncle.color == False:
+                    current.parent.color = True
+                    uncle.color = True
+                    current.parent.parent.color = False  # grandparent改成紅色
+                    current = current.parent.parent
+                # case2 & 3: uncle是黑色
+                else:
+                    if current == current.parent.left:  # case2
+                        current = current.parent
+                        self.right_rotation(current)
+                    # case3
+                    current.parent.color = True  # 把parent塗黑
+                    current.parent.parent.color = False  # grandparent塗紅
+                    self.left_rotation(current.parent.parent)
+
+        self.root.color = True  # 確保root是黑色
